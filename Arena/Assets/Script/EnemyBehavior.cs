24c52ad9db2ea50d25bdd4ee5c7a48e29beafe4c
Dylan <dylanhoffman651@gmail.com>
2021-02-02 20:28:51 -0700

    3 Pickups

---
+++ Arena/Assets/Script/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player") ;
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+}
---
+++ Arena/Assets/Script/ItemBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player");
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Item collected!");
+        }
+
+    }
+}
